3f650bad6ab0bf4258496bb584594d930c49d207
The_Blank.eth <103093516+Markxethx@users.noreply.github.com>
2022-09-14 22:29:42 +0800

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function getRandomNumber(){
     return Math.floor(Math.random() * randomHex.length)
 }
 
-const tip = document.querySelector('.tip')
+const tip = document.querySelector('.tip');
 tip.addEventListener('click', function(){
     let hexColor = "#";
 
@@ -24,12 +24,12 @@ tip.addEventListener('click', function(){
      
     
     tip.style.background = `linear-gradient(to top, ${hexColor}, ${hexColors})`;
-
+    tipMe.style.color = hexColor; 
 });
 
 /*tipHolder */
 
-const tipHolder = document.querySelector('.tipHolder')
+const tipHolder = document.querySelector('.tipHolder');
 tipHolder.addEventListener('click', function(){
     let hexColor = "#";
 
@@ -47,12 +47,12 @@ tipHolder.addEventListener('click', function(){
      
     
     tipHolder.style.background = `linear-gradient(to bottom, ${hexColor}, ${hexColors})`;
-
+    tipHolderMe.style.color = hexColor;
 });
 
 /*atomizer */
 
-const atomizer = document.querySelector('.atomizer')
+const atomizer = document.querySelector('.atomizer');
 atomizer.addEventListener('click', function(){
     let hexColor = "#";
 
@@ -70,13 +70,13 @@ atomizer.addEventListener('click', function(){
      
     
     atomizer.style.background = `linear-gradient(to top, ${hexColor}, ${hexColors})`;
-  
+    atomizerMe.style.color = hexColor;
 
 });
 
 /*body */
 
-const body = document.querySelector('.body')
+const body = document.querySelector('.body');
 body.addEventListener('click', function(){
     let hexColor = "#";
 
@@ -95,17 +95,28 @@ body.addEventListener('click', function(){
      
     
     body.style.background = `linear-gradient( ${hexColor}, ${hexColors})`;
-
+    bodyMe.style.color = hexColor;
 });
 
 
-const switchA = document.querySelector('.switchA')
-const switchRgb= document.querySelector('.switchRgb')
-const switchMe = document.querySelector('.clickMe')
-const switchMeToo = document.querySelector('.clickMeToo')
+const switchA = document.querySelector('.switchA');
+const switchRgb= document.querySelector('.switchRgb');
+const switchMe = document.querySelector('.clickMe');
+const switchMeToo = document.querySelector('.clickMeToo');
+const crossFather = document.querySelector('.crossFather');
+const touch = document.querySelector('.touch');
+const touchTwo = document.querySelector('.touchTwo');
+const touchMe = document.querySelector('.touchMe')
+const touchTwoMe = document.querySelector('.touchTwoMe');
+const geekVape = document.querySelector('.geekVape');
+const tipMe = document.querySelector('.tipMe');
+const tipHolderMe = document.querySelector('.tipHolderMe');
+const atomizerMe = document.querySelector('.atomizerMe');
+const bodyMe = document.querySelector('.bodyMe');
+const triangle = document.querySelector('.triangle');
+const boxes= document.querySelector('.boxes');
 
-
-const container = document.querySelector('.container')
+const container = document.querySelector('.container');
 
 let index = 0;
 let a = "white";
@@ -118,17 +129,147 @@ switchA.addEventListener('click', function(){
       container.style.background = colors[index];
 
       switchA.style.color = colors[index + 1];
-      switchRgb.style.color = colors[index + 1];
-      switchMe.style.color = colors[index + 1];
-      switchMeToo.style.color = colors[index + 1];
-    
+
       index = index >= colors.length - 1 ? 100 : index + 1;
       
 
 });
 
-const switchRgbb = document.querySelector('.switchRgb')
-switchRgbb.addEventListener('click', function(){
+/* tip */
+
+tipMe.addEventListener('click', function(){
+    let hexColor = "#";
+
+    for (a = 0; a < 6; a ++) {
+
+        hexColor += randomHex[getRandomNumber()]
+    } 
+
+    
+    let hexColors = "#";
+
+    for (a = 0; a < 6; a ++) {
+
+        hexColors += randomHex[getRandomNumber()]
+    } 
+     
+    tip.style.background = `linear-gradient(to top, ${hexColor}, ${hexColors})`;
+    tipMe.style.color = hexColor
+});
+
+tipHolderMe.addEventListener('click', function(){
+    let hexColor = "#";
+
+    for (a = 0; a < 6; a ++) {
+
+        hexColor += randomHex[getRandomNumber()]
+    } 
+
+    
+    let hexColors = "#";
+
+    for (a = 0; a < 6; a ++) {
+
+        hexColors += randomHex[getRandomNumber()]
+    } 
+     
+    tipHolder.style.background = `linear-gradient(to top, ${hexColor}, ${hexColors})`;
+    tipHolderMe.style.color = hexColor
+});
+
+atomizerMe.addEventListener('click', function(){
+    let hexColor = "#";
+
+    for (a = 0; a < 6; a ++) {
+
+        hexColor += randomHex[getRandomNumber()]
+    } 
+
+    
+    let hexColors = "#";
+
+    for (a = 0; a < 6; a ++) {
+
+        hexColors += randomHex[getRandomNumber()]
+    } 
+     
+    atomizer.style.background = `linear-gradient(to top, ${hexColor}, ${hexColors})`;
+    atomizerMe.style.color = hexColor
+});
+
+bodyMe.addEventListener('click', function(){
+    let hexColor = "#";
+
+    for (a = 0; a < 6; a ++) {
+
+        hexColor += randomHex[getRandomNumber()]
+    } 
+
+    
+    let hexColors = "#";
+
+    for (a = 0; a < 6; a ++) {
+
+        hexColors += randomHex[getRandomNumber()]
+    } 
+     
+    body.style.background = `linear-gradient(to top, ${hexColor}, ${hexColors})`;
+    bodyMe.style.color = hexColor
+});
+
+crossFather.addEventListener('click', function(){
+    cross1 = document.querySelector('.cross1')
+    cross2 = document.querySelector('.cross2')
+    cross3 = document.querySelector('.cross3')
+    cross4 = document.querySelector('.cross4')
+    let hexColor = "#";
+
+    for (a = 0; a < 6; a ++) {
+
+        hexColor += randomHex[getRandomNumber()]
+    } 
+
+    cross1.style.color = hexColor;
+    cross2.style.color = hexColor;
+    cross3.style.color = hexColor;
+    cross4.style.color = hexColor;
+    crossFather.style.color = hexColor;
+    console.log("hello")
+});
+
+geekVape.addEventListener('click', function(){
+    geek = document.querySelector('.geek');
+    vape = document.querySelector('.vape')
+    let hexColor = "#";
+
+    for (a = 0; a < 6; a ++) {
+
+        hexColor += randomHex[getRandomNumber()]
+    } 
+     
+    geek.style.color = hexColor;
+    vape.style.color = hexColor;
+    geekVape.style.color = hexColor;
+});
+
+
+triangle.addEventListener('click', function(){
+
+    let hexColor = "#";
+
+    for (a = 0; a < 6; a ++) {
+
+        hexColor += randomHex[getRandomNumber()]
+    } 
+     
+    touch.style.borderLeft = "10px " +  "solid " + hexColor;
+    touchTwo.style.borderLeft = "10px " +  "solid " + hexColor;
+    touchMe.style.borderLeft = "10px " + "solid " + hexColor;
+    touchTwoMe.style.borderLeft = "10px " + "solid " + hexColor;
+    triangle.style.color = hexColor
+});
+
+switchRgb.addEventListener('click', function(){
     let hexColor = "#";
 
     for (a = 0; a < 6; a ++) {
@@ -146,10 +287,23 @@ switchRgbb.addEventListener('click', function(){
      
     
     container.style.background = `linear-gradient( ${hexColor}, ${hexColors})`;
-
+    switchRgb.style.color = hexColors;
 });
 
-const clickMee = document.querySelector('.clickMe')
+boxes.addEventListener('click', function(){
+    let hexColor = "#";
+
+    for (a = 0; a < 6; a ++) {
+
+        hexColor += randomHex[getRandomNumber()]
+    } 
+    
+    body.style.borderTop = '5px ' + 'solid ' + hexColor;
+    body.style.borderBottom = '5px ' + 'solid ' + hexColor;
+    boxes.style.color = hexColor;
+});
+
+const clickMee = document.querySelector('.clickMe');
 clickMee.addEventListener('click', function(){
     headBox = document.querySelector('.headBox')
     firstBox = document.querySelector('.firstBox')
@@ -176,7 +330,7 @@ clickMee.addEventListener('click', function(){
     switchMe.style.color = hexColor ;
 });
 
-const clickMeeToo = document.querySelector('.clickMeToo')
+const clickMeeToo = document.querySelector('.clickMeToo');
 clickMeeToo.addEventListener('click', function(){
     babyBox = document.querySelector('.babyBox')
     babyBoxHead = document.querySelector('.babyBoxHead')
